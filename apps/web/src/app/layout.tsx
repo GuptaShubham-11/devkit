e894b5c3c31f50aa71780083ca95eedf14e50d19
Gupta Shubham <164208744+GuptaShubham-11@users.noreply.github.com>
2026-04-05 10:41:12 +0530

    ref(layout): change poppins to inter and add dark classname (#2)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-poppins",
+  variable: "--font-inter",
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
@@ -19,10 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning lang="en">
-      <body className={`${inter.variable} antialiased`}>
-        {children}
-      </body>
+    <html suppressHydrationWarning className="dark" lang="en">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
