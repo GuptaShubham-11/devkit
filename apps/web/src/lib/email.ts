e6eb7f7d7280c6f3d3e1fd0c01825b65001a5edb
Gupta Shubham <164208744+GuptaShubham-11@users.noreply.github.com>
2026-04-08 07:17:41 +0530

    feat(email): add email (#20)

---
+++ apps/web/src/lib/email.ts
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer";
+
+if (!process.env.EMAIL_USER || !process.env.APP_PASSWORD) {
+  throw new Error("EMAIL_USER and APP_PASSWORD must be set!");
+}
+
+export function sendEmail({
+  emailAddress,
+  emailSubject,
+  htmlText,
+}: {
+  emailAddress: string;
+  emailSubject: string;
+  htmlText: string;
+}) {
+  try {
+    const mailConfig = {
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.APP_PASSWORD,
+      },
+    };
+
+    const transporter = nodemailer.createTransport(mailConfig);
+
+    const mailDetails = {
+      from: "Devkit",
+      to: emailAddress,
+      subject: emailSubject,
+      html: htmlText,
+    };
+
+    transporter.sendMail(mailDetails);
+  } catch (error) {
+    // console.error('Nodemailer send mail error', error);
+    return error;
+  }
+}
