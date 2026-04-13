8435a4ea8c448d0ee7e690d5d98bfc7d90d003ba
GuptaShubham-11 <shu919847@gmail.com>
2026-04-13 08:54:32 +0530

    ref(axios): add axios instance

--- packages/shared/src/axios-instance/index.ts
+++ packages/shared/src/axios-instance/index.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
-import "dotenv/config";
 
-export const http = axios.create({
-  baseURL: process.env.API_BASE_URL,
+export const axiosInstance = axios.create({
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
