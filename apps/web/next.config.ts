1dc620ac6a7c4f1086ec9582b69b78f72f7facdc
Gupta Shubham <164208744+GuptaShubham-11@users.noreply.github.com>
2026-05-03 08:51:23 +0530

    ref(app): add scrollbar globals css and ref next config (#64)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "ik.imagekit.io/l2zuz3xzb/devkit",
+        hostname: "ik.imagekit.io",
       },
     ],
   },
