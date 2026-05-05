3b78a7324a2e24647e1b5408f31911fbf4540127
GuptaShubham-11 <shu919847@gmail.com>
2026-05-05 21:02:54 +0530

    ref(turbo): add env variables

--- apps/web/src/lib/http.ts
+++ apps/web/src/lib/http.ts
@@ -2,10 +2,10 @@ import { axiosInstance } from "@repo/shared";
 
 import { secretVariables } from "./secret-variables";
 
-const { API_BASE_URL } = secretVariables();
+const { NEXT_PUBLIC_API_BASE_URL } = secretVariables();
 
 const http = axiosInstance.create({
-  baseURL: API_BASE_URL,
+  baseURL: NEXT_PUBLIC_API_BASE_URL,
 });
 
 export { http };
